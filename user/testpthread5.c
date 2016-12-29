b9f8b7e9b5b8695201a1e4328a3d17e254cabe92
Alexey Khoroshilov <khoroshilov@ispras.ru>
2016-12-29 17:16:38 +0300

    test5 updated

--- user/testpthread5.c
+++ user/testpthread5.c
@@ -2,28 +2,40 @@
 //JOIN before the target pthread is finished.
 #include <inc/lib.h>
 void *func1(void *a) {
-  sys_yield();
   int *res;
   res = malloc(4);
   *res = 777;
-  cprintf("I am POSIX thread! number [%08x] and I send value: %d\n", *(pthread_t*)a, *res);
+  cprintf("1: I am POSIX thread! number [%08x] and I send value: %d\n", *(pthread_t*)a, *res);
   return (void*)res;
 }
 
 void *func2(void *a) {
   int *res;
-  cprintf("I am POSIX thread! And I join [%08x]\n", *(pthread_t*)a);
-  cprintf("I'am joining\n");
+  cprintf("2: I am POSIX thread! And I join [%08x]\n", *(pthread_t*)a);
+  cprintf("2: I'am joining\n");
   sys_pthread_join(*(pthread_t*)a, (void**)&res);
-  cprintf("I'VE GOT RESULT OF FIRST PTHREAD: %d\n", *res);
+  cprintf("2: I'VE GOT RESULT OF FIRST PTHREAD: %d\n", *res);
   return NULL;
 }
 void
 umain(int argc, char **argv)
 {
   pthread_t t1, t2;
+  sys_sched_setscheduler(0, SCHED_FIFO, 1);
+  sys_print_pthread_state(0);
   sys_pthread_create(&t1, NULL, &func1, &t1);
   sys_pthread_create(&t2, NULL, &func2, &t1);
+
+  cprintf("0: -----------------1\n");
+  sys_print_pthread_state(t1);
+  sys_print_pthread_state(t2);
+  cprintf("0: -----------------2\n");
+  sys_sched_setscheduler(t2, SCHED_FIFO, 4);
+  cprintf("0: -----------------3\n");
+  sys_print_pthread_state(t1);
+  sys_print_pthread_state(t2);
+  cprintf("0: -----------------4\n");
+
   sys_yield();
   sys_yield();
 }
